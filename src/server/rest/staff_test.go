1392114f3ebb327e4c335f9adceaf7eca35c66d7
ketsu.kan <ketsu.kan.jp@gmail.com>
2022-01-20 13:33:40 +0900

    add unit test

--- src/server/rest/staff_test.go
+++ src/server/rest/staff_test.go
@@ -7,16 +7,22 @@ import (
 
 func Test_GetStaffAll(t *testing.T) {
 	ary := GetStaffAll()
-	isEqual := len(ary) == len(mock.MultiStaff)
+	isEqual := len(ary) == len(mock.MultiStaff)+1
 	if isEqual {
-		for i := 0; i < len(ary); i++ {
-			if ary[i] != mock.MultiStaff[i] {
+		if ary[0] != mock.OneStaff {
+			isEqual = false
+		}
+	}
+	if isEqual {
+		for i := 0; i < len(mock.MultiStaff); i++ {
+			if ary[i+1] != mock.MultiStaff[i] {
 				isEqual = false
 				break
 			}
 		}
 	}
 	if !isEqual {
+		t.Errorf("get=%v, org=%v", ary, mock.MultiStaff)
 		t.Errorf("rest.GetStaffAll() failed;")
 	} else {
 		t.Log("rest.GetStaffAll() is ok")
